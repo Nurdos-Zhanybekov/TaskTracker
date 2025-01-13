e85b2545f2661340f95e12c06d2c97d623659136
Nurdos Zhanybekov <nurdoszhanybekov.w@gmail.com>
2025-01-13 11:13:27 +0600

    Added methods in TaskManagement for sorting tasks by priority or status. Also methods for displaying amount of tasks, amount tasks those are completed or pending, and method for counting tasks by priority

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<TaskInfo> taskInfos = new ArrayList<>();
-        TaskInfo taskInfo = new TaskInfo(12, "fdsafdas", "High", "Pending");
+        TaskInfo taskInfo = new TaskInfo(12, "fdsafdas", "Medium", "Pending");
         TaskInfo taskInfo1 = new TaskInfo(13, "fdsafd", "High", "Completed");
+        TaskInfo taskInfo2 = new TaskInfo(14, "fdsafdasf", "High", "Pending");
         taskInfos.add(taskInfo);
         taskInfos.add(taskInfo1);
+        taskInfos.add(taskInfo2);
 
         TaskManagement taskManagement = new TaskManagement(taskInfos);
-        taskManagement.updateStatus(12);
-        taskManagement.searchPriority("High");
+        taskManagement.displayAmountTasks();
+        taskManagement.displayAmountCompletedOrPendingTasks();
+        System.out.println(taskManagement.countPriority(taskInfos, "High"));
 
     }
 }
--- src/TaskManagement.java
+++ src/TaskManagement.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class TaskManagement {
     private final ArrayList<TaskInfo> taskInfoList;
+    private static int countTasks;
+    private static int countCompletedTasks;
+    private static int countPendingTasks;
+    private static int countHighPriority;
+    private static int countMediumPriority;
+    private static int countEasyPriority;
 
     public TaskManagement(ArrayList<TaskInfo> taskInfoList){
         this.taskInfoList = taskInfoList;
@@ -47,10 +54,104 @@ public class TaskManagement {
     public void deleteTask(int id){
         for(TaskInfo taskInfo : taskInfoList) {
             for (int i : taskInfo.getAllIDs()) {
-                if (i == id) {
-                    taskInfo.getAllIDs().remove(i);
+                if (i == id && i == taskInfo.getId()) {
+                    taskInfoList.remove(taskInfo);
+                    break;
                 }
             }
         }
+    }
+
+    public void prioritySort(){
+        for(TaskInfo taskInfo : taskInfoList){
+            for(String priority : taskInfo.getPriorityList()){
+                if(priority.equalsIgnoreCase("High") && priority.equalsIgnoreCase(taskInfo.getPriority())){
+                    taskInfo.getDetails();
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+
+        for(TaskInfo taskInfo : taskInfoList) {
+            for (String priority : taskInfo.getPriorityList()) {
+                if (priority.equalsIgnoreCase("Medium") && priority.equalsIgnoreCase(taskInfo.getPriority())) {
+                    taskInfo.getDetails();
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+
+        for(TaskInfo taskInfo : taskInfoList) {
+            for (String priority : taskInfo.getPriorityList()) {
+                if (priority.equalsIgnoreCase("Low") && priority.equalsIgnoreCase(taskInfo.getPriority())) {
+                    taskInfo.getDetails();
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void statusSort(){
+        for(TaskInfo taskInfo : taskInfoList){
+            for(int i : taskInfo.getPendingList()){
+                if(i == taskInfo.getId()){
+                    taskInfo.getDetails();
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+
+        for(TaskInfo taskInfo : taskInfoList){
+            for(int i : taskInfo.getCompletedList()){
+                if(i == taskInfo.getId()){
+                    taskInfo.getDetails();
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void displayAmountTasks(){
+        for(TaskInfo taskInfo : taskInfoList){
+            countTasks++;
+        }
+
+        System.out.println("Amount of Tasks: " + countTasks);
+    }
+
+    public void displayAmountCompletedOrPendingTasks(){
+        for(TaskInfo taskInfo : taskInfoList){
+            for(int i : taskInfo.getCompletedList()){
+                if(i == taskInfo.getId()){
+                    countCompletedTasks++;
+                    break;
+                }
+            }
+        }
+
+        for(TaskInfo taskInfo : taskInfoList){
+            for(int i : taskInfo.getPendingList()){
+                if(i == taskInfo.getId()){
+                    countPendingTasks++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Amount of Completed Tasks: " + countCompletedTasks);
+        System.out.println("Amount of Pending Tasks: " + countPendingTasks);
+    }
+
+    public int countPriority(List<TaskInfo> taskInfos, String priority){
+        if(taskInfos.isEmpty()) return 0;
+
+        TaskInfo firstTaskInfo = taskInfos.getFirst();
+        int count = firstTaskInfo.getPriority().equalsIgnoreCase(priority) ? 1 : 0;
+        return count + countPriority(taskInfos.subList(1, taskInfos.size()), priority);
     }
 }
